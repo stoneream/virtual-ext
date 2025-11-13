70ff163295b66ae08a85be7c765f4250803dfc4a
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2025-11-14 00:49:32 +0900

    動かないゴミを一掃

--- header.js
+++ header.js
@@ -3,19 +3,3 @@
 include("diff.js");
 include("logger.js");
 include("live-object-observer.js");
-
-// Supervisor Classes
-
-include("dispatcher.js");
-include("event_types.js");
-
-// Binding Classes
-
-include("binding_device_menu.js");
-include("binding_device.js");
-include("binding_knob_group.js");
-include("binding_live_set.js");
-include("binding_parameter.js");
-include("binding_parameter_menu.js");
-include("binding_track_menu.js");
-include("binding_track.js");
